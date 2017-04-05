2ac7ba38ab58cfb3d776ff8a52c5ff5c2a434a0b
Michael Terry <michael@michaelterry.org>
2017-04-05 18:40:54 +0000

    Set js indent to 2

--- init.el
+++ init.el
@@ -290,6 +290,7 @@
   :config
   (progn
     (add-hook 'js2-mode-hook (lambda () (setq mode-name "js2")))
+    (add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
     (setf js2-skip-preprocessor-directives t)
     (setq-default js2-additional-externs
                   '("$" "unsafeWindow" "localStorage" "jQuery"
